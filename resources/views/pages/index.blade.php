70890baa8f7b30b1c538b940862ac390b83d9f7d
Agung Sepruloh <agung.10118902@mahasiswa.unikom.ac.id>
2019-05-14 20:37:17 +0700

    Added laravel files and user authentication

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <div class="jumbotron text-center">
+        <h1>URL Shortener</h1>
+        <p>This is laravel application for urls</p>
+    </div>
+
+@endsection
